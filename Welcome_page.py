711401cc4e0d83c68198cbae263acf2d707685df
Snetter <136588346+Sondreskj@users.noreply.github.com>
2023-12-14 12:50:54 +0000

    mods to welcome_page

--- Welcome_page.py
+++ Welcome_page.py
@@ -24,7 +24,7 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to migration shourcut world! 👋")
+    st.write("# Welcome to migration shortcut world! 👋")
 
     st.sidebar.success("Select a demo above.")
 
